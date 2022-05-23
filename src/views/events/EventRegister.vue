dbe7b695aef76248202011199b8988aa2a86f1bc
artbog <a.boguslavsky@itransition.com>
2022-05-23 19:36:04 +0300

    Touring Vue Router - lesson 6

--- src/views/events/EventRegister.vue
+++ src/views/events/EventRegister.vue
@@ -1,9 +1,19 @@
 <template>
   <p>Register for event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      // Call to API
+
+      this.$router.push({
+        name: 'EventDetails',
+      })
+    },
+  },
 }
 </script>
